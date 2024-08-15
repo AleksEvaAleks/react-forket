dece73a893c5d561e8efac6bf1951e7ed77ffe5f
AleksEvaAleks <166129085+AleksEvaAleks@users.noreply.github.com>
2024-08-15 14:25:05 +0300

    Add files via upload

---
+++ precache-manifest.9f1ad80493498a3614167bd4d270b93d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdafb6db188a05831a474128eb611935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dc7395ee87cdca011c8",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "d06c2beb3975465bb472",
+    "url": "/static/js/2.e45e020e.chunk.js"
+  },
+  {
+    "revision": "4dc7395ee87cdca011c8",
+    "url": "/static/js/main.a2713aa3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
